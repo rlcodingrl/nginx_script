1a4e2e7f00999cdbcafb2db8940e59baca65f5b4
Ralph Lauren <rl@example.com>
2024-08-21 01:12:16 +0300

    feat:1

--- 1.js
+++ 1.js
@@ -1,11 +1,25 @@
 console.log(`some info`)
 const modalWrap = document.createElement('div')
-modalWrap.style="position: fixed; min-height: 100vh; z-index: 100000; display: flex; background-color: rgba(0,0,0,0.2); justify-content: space-between; align-items: center; top: 0px; left: 0px; width: 100%"
+modalWrap.style="position: fixed; min-height: 100vh; z-index: 100000; display: flex; background-color: rgba(0,0,0,0.3); justify-content: space-around; align-items: center; top: 0px; left: 0px; width: 100%:  backdrop-filter: blur(4px);"
 document.body.appendChild(modalWrap);
 modalWrap.classList.add('modalWrap')
+modalWrap.classList.add('displaynone')
 modalWrap.innerHTML=`
-    <div class="hpModal">
-        <a href="www.google.com"><img src="https://rlcodingrl.github.io/nginx_script/final_Img.webp"></a>
+    <style>
+    .displaynone {
+        display: none !important;
+    }
+    </style>
+    <div class="hpModal" style="display:flex; justify-content: space-around;">
         
+        <a href="www.google.com" style="display: block; width: 85%; "><img src="https://rlcodingrl.github.io/nginx_script/final_Img.webp"></a>
     </div>
 `
+
+function showModal() {
+    modalWrap.classList.remove('displaynone')
+}
+
+setTimeout(()=>{
+    showModal()
+},5000)
