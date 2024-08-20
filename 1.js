5e4aa3b51c56bd485a474e770cbf2f53803a18ef
Ralph Lauren <rl@example.com>
2024-08-20 23:11:14 +0300

    feat:1

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 console.log(`some info`)
 const modalWrap = document.createElement('div')
-modalWrap.style="position: fixed; min-height: 100vh; z-index: 100000; display: flex; background-color: rgba(0,0,0,0.2); justify-content: space-between; align-items: center"
+modalWrap.style="position: fixed; min-height: 100vh; z-index: 100000; display: flex; background-color: rgba(0,0,0,0.2); justify-content: space-between; align-items: center; top: 0px; left: 0px; width: 100%"
 document.body.appendChild(modalWrap);
 modalWrap.classList.add('modalWrap')
 modalWrap.innerHTML=`
