1ee079ef907ef12d9746344779b9fa35111785b7
Ralph Lauren <rl@example.com>
2024-09-10 02:23:31 +0300

    featL artsy

--- artsy.js
+++ artsy.js
@@ -44,25 +44,6 @@ console.log('this is artsy js from rl github')
 
 
         console.log(4);
-
-        // Удаление всех событий, зарегистрированных через window.onbeforeunload
-window.onbeforeunload = null;
-
-// Если вы знаете обработчик, удалите его через removeEventListener
-window.removeEventListener('beforeunload', beforeUnloadHandler);
-
-
-        // for (let i = 0; i < buttons.length; i++) {
-        //     const divs = buttons[i].getElementsByTagName("div");
-        //     if (divs) {
-        //         for (let j = 0; j < divs.length; j++) {
-        //             if (divs[j].textContent.trim() === "Save and Continue") {
-        //                 console.log(`Found , ${buttons[i]}`);
-        //                 console.log(buttons[i]);
-                        
-        //                 break;
-        //             }
-        //         }
-        //     }
-
-        // }
+        window.alert = function() {
+            // Ничего не делаем — переопределяем alert
+        };
