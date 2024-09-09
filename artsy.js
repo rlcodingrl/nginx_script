9fb049fb8217db916d6bb6850173663568542a8e
Ralph Lauren <rl@example.com>
2024-09-09 20:54:21 +0300

    featL artsy

--- artsy.js
+++ artsy.js
@@ -3,17 +3,17 @@ console.log('this is artsy js from rl github')
    console.log("script from rl-www");
         const buttons = document.getElementsByTagName("button");
         console.log(2);
-        for (let i = 0; i < buttons.length; i++) {
-            const divs = buttons[i].getElementsByTagName("div");
+        // for (let i = 0; i < buttons.length; i++) {
+        //     const divs = buttons[i].getElementsByTagName("div");
 
-            for (let j = 0; j < divs.length; j++) {
-                if (divs[j].textContent.trim() === "Save and Continue") {
-                    console.log(`Found , ${buttons[i]}`);
-                    buttons[i].addEventListener(("click", () => {
-                        window.location.href = "https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg";
-                    }))
-                    break;
-                }
-            }
-        }
-        console.log(4):
+        //     for (let j = 0; j < divs.length; j++) {
+        //         if (divs[j].textContent.trim() === "Save and Continue") {
+        //             console.log(`Found , ${buttons[i]}`);
+        //             buttons[i].addEventListener(("click", () => {
+        //                 window.location.href = "https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg";
+        //             }))
+        //             break;
+        //         }
+        //     }
+        // }
+        console.log(4);
