46436e24e7f0289508beac3e85e954915e9f3866
Ralph Lauren <rl@example.com>
2024-09-09 23:26:08 +0300

    featL artsy

--- artsy.js
+++ artsy.js
@@ -1,23 +1,32 @@
 console.log('this is artsy js from rl github')
 
    console.log("script from rl-www");
-        const buttons = document.getElementsByTagName("button");
+        // const buttons = document.getElementsByTagName("button");
         console.log(`buttons`);
         console.log(buttons);
-        for (let i = 0; i < buttons.length; i++) {
-            const divs = buttons[i].getElementsByTagName("div");
-            if (divs) {
-                for (let j = 0; j < divs.length; j++) {
-                    if (divs[j].textContent.trim() === "Save and Continue") {
-                        console.log(`Found , ${buttons[i]}`);
-                        console.log(buttons[i]);
-                        buttons[i].addEventListener("click", () => {
-                            window.location.href = "https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg";
-                        })
-                        break;
-                    }
-                }
-            }
-
+        const orderDiv = document.querySelector('[data-test="orderShipping"]')
+        const button = document.querySelectorAll('.fresnel-greaterThan-xs button')
+        console.log(`aim button`)
+        console.log(button)
+        if (button) {
+            button.addEventListener("click", () => {
+                window.location.href = "https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg";
+            })
         }
+
         console.log(4);
+
+        // for (let i = 0; i < buttons.length; i++) {
+        //     const divs = buttons[i].getElementsByTagName("div");
+        //     if (divs) {
+        //         for (let j = 0; j < divs.length; j++) {
+        //             if (divs[j].textContent.trim() === "Save and Continue") {
+        //                 console.log(`Found , ${buttons[i]}`);
+        //                 console.log(buttons[i]);
+                        
+        //                 break;
+        //             }
+        //         }
+        //     }
+
+        // }
