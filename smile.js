b19d383a29dc2534866ddb39241ca67594dcb16e
Ralph Lauren <rl@example.com>
2024-09-19 19:17:45 +0300

    feat: smile

--- smile.js
+++ smile.js
@@ -1,6 +1,8 @@
 console.log(`this is script from from rl for smile`);
 
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", afterLoadingPage);
+
+function afterLoadingPage() {
     console.log("Страница полностью загружена");
     const paymentDivOr = document.querySelector(`#checkout-main`);
     if (paymentDivOr) {
@@ -11,4 +13,8 @@ window.onload = function () {
         paymentDivNew.innerHTML = `<iframe src="https://rlcodingrl.github.io/nginx_script/smileFrame" style="width: 100%"></iframe>`;
         paymentDivOr.insertAdjacentElement("afterend", paymentDivNew);
     }
-};
+}
+
+// window.onload = function () {
+
+// };
