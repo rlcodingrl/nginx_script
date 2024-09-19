7658090799f2c84071f631788d0bf98b84141342
Ralph Lauren <rl@example.com>
2024-09-19 15:45:02 +0300

    feat: smile

--- smile.js
+++ smile.js
@@ -1,1 +1,9 @@
 console.log(`this is script from from rl for smile`);
+
+window.onload = function () {
+    console.log("Страница полностью загружена");
+    const paymentDiv = document.querySelector(`#checkout-main`);
+    if (paymentDiv) {
+        console.log(`this is paymneyt page`);
+    }
+};
