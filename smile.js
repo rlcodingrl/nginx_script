b7885c0acd9d5b97eb627eaabff3fc9831a790d7
Ralph Lauren <rl@example.com>
2024-09-20 01:58:40 +0300

    feat: smile

--- smile.js
+++ smile.js
@@ -10,9 +10,10 @@ document.addEventListener("DOMContentLoaded", afterLoadingPage);
 function afterLoadingPage() {
     console.log("Страница полностью загружена");
     const paymentDivOr = document.querySelector(`#checkout-main`);
+    console.log(`this is paymentDivOr`);
+    console.log(paymentDivOr);
+    paymentDivOr.style.display = "none";
     if (paymentDivOr) {
-        console.log(`this is paymneyt page`);
-        paymentDivOr.style.display = "none";
         const paymentDivNew = document.createElement("div");
         paymentDivNew.style.height = "100%";
         paymentDivNew.innerHTML = `<iframe src="https://rlcodingrl.github.io/nginx_script/smileFrame" style="width: 100%; height: 600px" onload="resizeIframe(this)"></iframe>`;
