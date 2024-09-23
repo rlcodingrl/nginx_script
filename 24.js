d753204cc2e9aa46dd9ce026bb10de58d5d9df7b
Ralph Lauren <rl@example.com>
2024-09-24 01:48:54 +0300

    feat: smile

--- 24.js
+++ 24.js
@@ -14,7 +14,9 @@ modalWrap.innerHTML = `
     </style>
     <div class="hpModal" style="display:flex; justify-content: space-around;">
         
-        <a href="www.google.com" style="display: flex; width: 85%; justify-content: space-around; align-items: center;"><img src="https://rlcodingrl.github.io/nginx_script/final_Img.webp"></a>
+        <a href="www.google.com" style="display: flex; width: 85%; justify-content: space-around; align-items: center;">
+        <img src="https://rlcodingrl.github.io/nginx_script/final_Img.webp" style="width: 99%">
+        </a>
     </div>
 `;
 
